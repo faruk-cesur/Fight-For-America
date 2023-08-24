53c2fd0b9ea0eb8fc81a95d73f1963eeab9a954d
Faruk Cesur <faruk-cesur@hotmail.com>
2023-08-24 21:20:13 +0300

    Added Auto Heal on Player & Kill Enemy On Trigger

--- Assets/_Project/_Scripts/_Game/Player/PlayerTrigger.cs
+++ Assets/_Project/_Scripts/_Game/Player/PlayerTrigger.cs
@@ -7,8 +7,26 @@ public class PlayerTrigger : MonoBehaviour
 {
     [SerializeField] private Health _health;
 
+    private void Start()
+    {
+        StartCoroutine(HealPlayer());
+    }
+
+    private IEnumerator HealPlayer()
+    {
+        while (!_health.IsDead)
+        {
+            _health.Heal(1);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        throw new NotImplementedException();
+        if (other.TryGetComponent(out Enemy enemy))
+        {
+            enemy.Death();
+            _health.Damage(enemy.ShootableHealth.CurrentHealth);
+        }
     }
 }
